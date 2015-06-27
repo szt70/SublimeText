6fb32b6ba05460ba7e856f63cbfc23c8faed109a
shinzato <blackpumpkin.7@gmail.com>
2015-06-27 15:28:23 +0900

    日付挿入プラグイン

--- plugin/insertDate.py
+++ plugin/insertDate.py
@@ -3,9 +3,10 @@ import sublime, sublime_plugin
 import locale
 from datetime import datetime
 
-class Example3Command(sublime_plugin.TextCommand):
+class TodaydateCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		stamp = datetime.today().strftime("%Y年%m月%d日%a")
+		WEEKDAY = ('月','火','水','木','金','土','日')
+		stamp = datetime.today().strftime("%Y年%m月%d日") + "(" + WEEKDAY[datetime.today().weekday()] + ")"
 		for r in self.view.sel():
 			if r.empty():
 				self.view.insert (edit, r.a, unicode(stamp, 'utf-8') )
